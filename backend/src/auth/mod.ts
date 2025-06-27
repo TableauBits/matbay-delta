e94912b985e00d128fc161774cce78684432af69
Ithyx <lamidey.m@gmail.com>
2025-06-27 11:23:21 -0400

    Switch headers based auth to middleware approach for better reusability

--- backend/src/auth/mod.ts
+++ backend/src/auth/mod.ts
@@ -1,20 +1,26 @@
-import { type Request, type Response, Router } from "express";
+import { type NextFunction, type Request, type Response, Router } from "express";
 import { checkNil } from "../utils";
 import { tokenDevRouter, validateToken } from "./token";
 
-async function checkAuth(req: Request, res: Response) {
-    const idToken = checkNil(req.header("delta-auth"), "no token found in headers");
-    if (idToken.isErr()) {
-        res.sendStatus(400);
+async function ensureAuthMiddleware(req: Request, res: Response, next: NextFunction) {
+    const result = checkNil(req.header("delta-auth"), "no token found in headers");
+    if (result.isErr()) {
+        res.statusCode = 401;
+        res.send(result);
         return;
     }
 
-    const validation = await validateToken(idToken.unwrap());
+    const validation = await validateToken(result.unwrap());
     if (validation.isErr()) {
-        res.sendStatus(401);
+        res.statusCode = 401;
+        res.send(validation);
         return;
     }
 
+    next();
+}
+
+async function checkAuth(_req: Request, res: Response) {
     res.sendStatus(200);
 }
 
@@ -22,6 +28,6 @@ const authApiRouter = Router();
 const authDevRouter = Router();
 
 authDevRouter.use("/token", tokenDevRouter);
-authDevRouter.use("/check", checkAuth);
+authDevRouter.use("/check", ensureAuthMiddleware, checkAuth);
 
 export { authApiRouter, authDevRouter };
