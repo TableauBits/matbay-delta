80057897c28049108d9b38a154dcacc959d05fda
Ithyx <lamidey.m@gmail.com>
2025-09-05 19:37:30 -0400

    Move account creation to seperate endpoint

--- backend/src/auth/mod.ts
+++ backend/src/auth/mod.ts
@@ -1,5 +1,5 @@
 import { type NextFunction, type Request, type Response, Router } from "express";
-import { Ok, Option } from "oxide.ts";
+import { Err, Ok, Option } from "oxide.ts";
 
 import { createUser, getUserFromAuth, type User } from "../user/mod";
 import { HttpError, HttpStatus, sendResult } from "../utils";
@@ -37,15 +37,16 @@ async function ensureAuthMiddleware(req: Request, res: Response, next: NextFunct
     const authID = maybeAuthID.unwrap();
     const maybeUser = await getUserFromAuth(authID);
     if (maybeUser.isNone()) {
-        const newUser: User = {
-            id: "",
-            authID,
-            username: validation.unwrap()["nickname"] ?? authID,
-            description: "",
-            imageURL: validation.unwrap()["picture"] ?? "",
-            joinDate: new Date().toISOString(),
-        };
-        await createUser(newUser);
+        sendResult(
+            Err(
+                new HttpError(
+                    HttpStatus.Unauthorized,
+                    "auth token seems valid but account is unknown, finish registration using the appropriate endpoint",
+                ),
+            ),
+            res,
+        );
+        return;
     }
 
     next();
@@ -55,10 +56,60 @@ async function checkAuth(_req: Request, res: Response) {
     sendResult(Ok("Authentication valid"), res);
 }
 
+async function registerAccount(req: Request, res: Response) {
+    const headerFetch = Option(req.header("delta-auth")).okOr(
+        new HttpError(HttpStatus.Unauthorized, "no token found in headers"),
+    );
+    if (headerFetch.isErr()) {
+        sendResult(headerFetch, res);
+        return;
+    }
+
+    const validation = await validateToken(headerFetch.unwrap());
+    if (validation.isErr()) {
+        const error = validation.mapErr(
+            (err) => new HttpError(HttpStatus.Unauthorized, `failed to authenticate: ${err.message}`),
+        );
+        sendResult(error, res);
+        return;
+    }
+
+    const maybeAuthID = Option(validation.unwrap().sub).okOr(
+        new HttpError(
+            HttpStatus.UnprocessableContent,
+            "failed to authenticate: the decoded jwt does not provided the necessary 'aud' claim",
+        ),
+    );
+    if (maybeAuthID.isErr()) {
+        sendResult(maybeAuthID, res);
+        return;
+    }
+
+    const authID = maybeAuthID.unwrap();
+    const maybeUser = await getUserFromAuth(authID);
+    if (maybeUser.isSome()) {
+        sendResult(Err(new HttpError(HttpStatus.UnprocessableContent, "account is already registered")), res);
+        return;
+    }
+
+    const newUser: User = {
+        id: "",
+        authID,
+        username: validation.unwrap()["nickname"] ?? authID,
+        description: "",
+        imageURL: validation.unwrap()["picture"] ?? "",
+        joinDate: new Date().toISOString(),
+    };
+
+    await createUser(newUser);
+    sendResult(Ok("registration complete"), res);
+}
+
 const authApiRouter = Router();
 const authDevRouter = Router();
 
 authDevRouter.use("/token", tokenDevRouter);
 authDevRouter.use("/check", ensureAuthMiddleware, checkAuth);
+authDevRouter.use("/register", registerAccount);
 
 export { authApiRouter, authDevRouter };
