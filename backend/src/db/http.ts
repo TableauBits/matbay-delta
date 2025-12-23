c299ade000f413ef6f341956c1fbf8e015cc1a41
Ithyx <lamidey.m@gmail.com>
2025-12-22 22:15:24 -0500

    Rework database creation to use consola as the logger

--- backend/src/db/http.ts
+++ backend/src/db/http.ts
@@ -1,4 +1,6 @@
 import { Database } from "bun:sqlite";
+import consola from "consola";
+import { sql, type Logger } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { type Request, type Response, Router } from "express";
@@ -9,18 +11,24 @@ export const dbURL = Option(process.env["DATABASE_URL"]).expect(
     "environment variable DATABASE_URL not found but is mandatory, check `.env.template`",
 );
 
+class DBLogger implements Logger {
+    logQuery(query: string, params: unknown[]): void {
+        consola.debug({ query, params });
+    }
+}
+
 const sqlite = new Database(dbURL);
 const db = drizzle({
     client: sqlite,
     schema: schema,
-    logger: true,
+    logger: new DBLogger(),
 });
 
 // By default, drizzle does not enforce foreign keys so we have to enable it ourselves.
 // However, we need to disable it during migrations because they may involve dropping tables.
-sqlite.run("PRAGMA foreign_keys = OFF;");
+db.run(sql`PRAGMA foreign_keys = OFF;`);
 migrate(db, { migrationsFolder: "./drizzle" });
-sqlite.run("PRAGMA foreign_keys = ON;");
+db.run(sql`PRAGMA foreign_keys = ON;`);
 
 async function dump(_: Request, res: Response) {
     res.sendFile(dbURL);
