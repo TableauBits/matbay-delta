d635ba2cf259dc5038dad662d242d3d204d90318
hvdbm <hugo.vdbm@icloud.com>
2026-04-05 10:45:43 -0400

    Redirect to user profile from their name or icon (DELTA-101) (#100)

---
+++ frontend/src/app/components/utils/redirect-to-user-profile/redirect-to-user-profile.ts
@@ -0,0 +1,21 @@
+import { Component, inject, input } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../../../../../common/user';
+
+@Component({
+  selector: 'app-redirect-to-user-profile',
+  imports: [],
+  templateUrl: './redirect-to-user-profile.html',
+  styleUrl: './redirect-to-user-profile.scss',
+})
+export class RedirectToUserProfile {
+  user = input<User | null>();
+  imgWidth = input<number>(25);
+  imgHeight = input<number>(25);
+
+  private router = inject(Router);
+
+  redirectToUserPage(id: string): void {
+    this.router.navigate(['/users', id]);
+  }
+}
