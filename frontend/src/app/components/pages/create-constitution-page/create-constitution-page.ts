174feb310a76cba71f64c8ec8452d5eed8c30c7c
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2026-06-02 01:34:11 +0000

    Add CreateConstitutionPage component

---
+++ frontend/src/app/components/pages/create-constitution-page/create-constitution-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-constitution-page',
+  imports: [],
+  templateUrl: './create-constitution-page.html',
+  styleUrl: './create-constitution-page.scss',
+})
+export class CreateConstitutionPage {}
